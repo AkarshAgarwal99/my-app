6fdb103719bbfc4f8060167f21d2000f8b093840
AKARSH AGARWAL <48346472+AkarshAgarwal99@users.noreply.github.com>
2023-02-14 18:13:57 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,112 +1,109 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios, * as others from 'axios';
-const get_voices = async () => {
-  return [
-       { Id: "voice1", LanguageName: "English", Name: "Joanna" },
-       { Id: "voice2", LanguageName: "Spanish", Name: "Miguel" },
-       { Id: "voice3", LanguageName: "German", Name: "Hans" }
- ]
-  // var voices_output;
-  // var config = {
-  //   method: 'get',
-  //   maxBodyLength: Infinity,
-  //   url: 'http://127.0.0.1:5000/',
-  //   headers: {}
-  // };
-  // return await axios(config)
-  //   .then(function (response) {
-  //     //  console.log(JSON.stringify(response.data));
-  //     voices_output = JSON.stringify(response.data.voices);
-  //     // console.log("mylog",voices_output);
-  //     return voices_output
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   });
-    // return voices_output
+function App() {
+
+    const [voices, setVoices] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [file, setFile] = useState(null);
+    const [audioBuffer, setAudioBuffer] = useState(null);
+    const [selectedVoice, setSelectedVoice] = useState("voice1");
+
+    useEffect(() => {
+        getAllVoices();
+    }, []);
+
+    const handleVoiceChange = event => {
+        setSelectedVoice(event.target.value);
+    };
+    const handleFileChange = event => {
+        setFile(event.target.files[0]);
+      };
+
+    const getAllVoices = () => {
+        axios.get("http://localhost:5000/").then((response) => {
+            setVoices(response.data.voices);
+            setLoading(false);
+        });
+    };
+
+    const playAudio = () => {
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("voice", selectedVoice);
+    
+        fetch("polly", {
+          method: "POST",
+          body: formData
+        })
+          .then(response => response.arrayBuffer())
+          .then(arrayBuffer => {
+            setAudioBuffer(arrayBuffer);
+          });
+      };
+    
+      const uploadfile = async () => {
+        let formData = new FormData();
+        formData.append("file", file);
+        console.log("aagya");
+        axios.post('3.111.29.114:5000/polly', formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          }
+        });
+    
+      };
+    
+      const pauseAudio = () => {
+        setAudioBuffer(null);
+      };
+
+    if (isLoading) {
+        return <div className="App">Loading...</div>;
+    }
+    // return (
+    //     <>
+    //         <select name="voice" onChange={handleVoiceChange}>
+    //             {
+    //                 voices.map(voice => (
+    //                     <option key={voice.Id} value={voice.Id}>
+    //                         {voice.LanguageName} - {voice.Name}
+    //                     </option>
+    //                 ))
+    //             }
+    //         </select>
+    //     </>
+
+    // );
+    return (
+        <div>
+          <h1>PDF to Audio Converter</h1>
+          <form>
+            <input type="file" name="file" onChange={handleFileChange} />
+            <br />
+            <br />
+            <select name="voice" onChange={handleVoiceChange}>
+              {
+                // for(let i = 0; i<voices.length; i++){
+                //               <option key={voice.Id} value={voice.Id}>
+                //   {voice.LanguageName} - {voice.Name}
+                // </option>
+                // }
+              //   () => {
+              //   console.log("mylogsssss",voices)
+              
+               voices.map(voice => (
+                <option key={voice.Id} value={voice.Id}>
+                  {voice.LanguageName} - {voice.Name}
+                </option>
+              ))}
+            </select>
+            <br />
+            <br />
+            <input type="button" value="Play MP3" onClick={playAudio} />
+            <input type="button" value="Pause MP3" onClick={pauseAudio} />
+            <input type="button" value="Upload" onClick={uploadfile} />
+          </form>
+        </div>
+    );
 }
-const PDFToAudioConverter = () => {
-  // get all voices list
-  // const [voices, setVoices] = useState([
-  //   { Id: "voice1", LanguageName: "English", Name: "Joanna" },
-  //   { Id: "voice2", LanguageName: "Spanish", Name: "Miguel" },
-  //   { Id: "voice3", LanguageName: "German", Name: "Hans" }
-  // ]);
-  const [voices, setVoices] = useState(get_voices());
-
-  const [selectedVoice, setSelectedVoice] = useState("voice1");
-  const [file, setFile] = useState(null);
-  const [audioBuffer, setAudioBuffer] = useState(null);
-
-  const handleFileChange = event => {
-    setFile(event.target.files[0]);
-  };
-
-  const handleVoiceChange = event => {
-    setSelectedVoice(event.target.value);
-  };
-
-  const playAudio = () => {
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("voice", selectedVoice);
-
-    fetch("polly", {
-      method: "POST",
-      body: formData
-    })
-      .then(response => response.arrayBuffer())
-      .then(arrayBuffer => {
-        setAudioBuffer(arrayBuffer);
-      });
-  };
-
-  const pauseAudio = () => {
-    setAudioBuffer(null);
-  };
-  const uploadfile = async () => {
-    let formData = new FormData();
-    formData.append("file", file);
-    console.log("aagya");
-    axios.post('3.111.29.114:5000/polly', formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      }
-    });
-
-  };
-
-  return (
-    <div>
-      <h1>PDF to Audio Converter</h1>
-      <form>
-        <input type="file" name="file" onChange={handleFileChange} />
-        <br />
-        <br />
-        <select name="voice" onChange={handleVoiceChange}>
-          {
-            // for(let i = 0; i<voices.length; i++){
-            //               <option key={voice.Id} value={voice.Id}>
-            //   {voice.LanguageName} - {voice.Name}
-            // </option>
-            // }
-          //   () => {
-          //   console.log("mylogsssss",voices)
-          
-           voices.map(voice => (
-            <option key={voice.Id} value={voice.Id}>
-              {voice.LanguageName} - {voice.Name}
-            </option>
-          ))}
-        </select>
-        <br />
-        <br />
-        <input type="button" value="Play MP3" onClick={playAudio} />
-        <input type="button" value="Pause MP3" onClick={pauseAudio} />
-        <input type="button" value="Upload" onClick={uploadfile} />
-      </form>
-    </div>
-  );
-};
-
-export default PDFToAudioConverter;
+export default App;
